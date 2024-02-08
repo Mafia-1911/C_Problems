cc8ac37233e51a2606b8398f3bdc50bb684f418b
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-02-08 22:48:10 +0600

    Struct.c

--- Struct/Struct.c
+++ Struct/Struct.c
@@ -7,3 +7,18 @@ struct Employee {
     char StartingDate[20];
     double Salary;
 };
+int main() {
+    struct Employee employees[4];
+
+    // Read user input to fill up the array of employees
+    for (int i = 0; i < 4; i++) {
+        printf("Enter details for Employee %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%s", employees[i].Name);
+        printf("Date of Birth (DOB): ");
+        scanf("%s", employees[i].DOB);
+        printf("Starting Date: ");
+        scanf("%s", employees[i].StartingDate);
+        printf("Salary: ");
+        scanf("%lf", &employees[i].Salary);
+    }

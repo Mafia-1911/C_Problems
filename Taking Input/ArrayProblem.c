10d36a4b05784a94cc1178a89171ffc3a6708f20
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-11-10 23:55:53 +0600

    Update ArrayProblem.c

--- Taking Input/ArrayProblem.c
+++ Taking Input/ArrayProblem.c
@@ -1,1 +1,9 @@
 //https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/D
+#include<stdio.h>
+int main(){
+    int input;
+    scanf("%d",&input);
+    int arr[1000+5];
+    for(int i=0;i<1000+5;i++){
+        arr[i]=15;
+    }
